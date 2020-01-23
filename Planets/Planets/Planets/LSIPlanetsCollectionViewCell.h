c1bcd849fa9b2254eb6c4751f2408cf1638a49d0
Percy <percival.ngan@gmail.com>
2020-01-23 06:25:49 -0600

    Added collection view controller and connected it to the view controller

---
+++ Planets/Planets/Planets/LSIPlanetsCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  LSIPlanetsCollectionViewCell.h
+//  Planets
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPlanetsCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Planets/Planets/Planets/LSIPlanetsCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  LSIPlanetsCollectionViewCell.m
+//  Planets
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import "LSIPlanetsCollectionViewCell.h"
+
+@implementation LSIPlanetsCollectionViewCell
+
+@end
