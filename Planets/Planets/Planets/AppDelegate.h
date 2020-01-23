7eedd831eb54d2de152dc34af4d9e6f23ff16758
Percy <percival.ngan@gmail.com>
2020-01-23 05:46:10 -0600

    Added project starter files

---
+++ Planets/Planets/Planets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Planets
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Planets/Planets/Planets/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Planets
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
